1f4d5bb26288dad98819b26051bbe05d0b3f5def
Pragyan <pragyandas@msn.com>
2025-04-01 00:14:00 +0530

    add test for actor message processing across system restart

---
+++ test/e2e/actorsystem_restart_actor_resurrection_test.go
@@ -0,0 +1,92 @@
+package actorsystemtest
+
+import (
+	"context"
+	"fmt"
+	"sync/atomic"
+	"testing"
+	"time"
+
+	"github.com/pragyandas/hydra/actor"
+	"github.com/pragyandas/hydra/test/e2e/utils"
+)
+
+func TestActorSystemRestartActorResurrection(t *testing.T) {
+	testContext, cancel := context.WithTimeout(context.Background(), 60*time.Second)
+	system, close := utils.SetupTestActorsystem(t)
+	if err := system.Start(testContext); err != nil {
+		t.Fatalf("Failed to start actor system: %v", err)
+	}
+	defer func() {
+		system.Close(testContext)
+		close()
+		cancel()
+	}()
+
+	const (
+		numMessages            = 100
+		processingTime         = 100 * time.Millisecond
+		actorSystemRestartTime = 3 * time.Second
+	)
+
+	var processedCount atomic.Int32
+
+	system.RegisterActorType("test", actor.ActorTypeConfig{
+		MessageHandlerFactory: func(self *actor.Actor) actor.MessageHandler {
+			return func(msg []byte) error {
+				time.Sleep(processingTime)
+				processedCount.Add(1)
+				t.Logf("processed message %d", processedCount.Load())
+				return nil
+			}
+		},
+		MessageErrorHandler: func(err error, msg actor.Message) {
+			t.Errorf("failed to handle message: %v", err)
+		},
+	})
+
+	actorA, err := system.CreateActor("test", "actor-a")
+	if err != nil {
+		t.Errorf("failed to create actor: %v", err)
+	}
+
+	actorB, err := system.CreateActor("test", "actor-b")
+	if err != nil {
+		t.Errorf("failed to create actor: %v", err)
+	}
+
+	for i := 0; i < numMessages; i++ {
+		msg := []byte(fmt.Sprintf("message-%d", i))
+		if err := actorA.SendMessage("test", actorB.ID(), msg); err != nil {
+			t.Errorf("Failed to send message %d: %v", i, err)
+		}
+	}
+
+	time.Sleep(actorSystemRestartTime)
+
+	// Restart the actor system
+	system.Close(testContext)
+	t.Logf("actor system closed")
+
+	// Sleep to ensure the actor is dead (liveness entry is TTL'ed in 3 seconds)
+	time.Sleep(3 * time.Second)
+
+	system.Start(testContext)
+	t.Logf("actor system re-started")
+
+	// Wait for the actor to be resurrected
+	deadline := time.Now().Add(60 * time.Second)
+	for time.Now().Before(deadline) {
+		if processedCount.Load() == int32(numMessages) {
+			break
+		}
+		time.Sleep(100 * time.Millisecond)
+	}
+
+	if processedCount.Load() != int32(numMessages) {
+		t.Errorf("expected %d messages to be processed, but got %d", numMessages, processedCount.Load())
+		return
+	}
+
+	t.Logf("Successfully processed all %d messages with actor resurrection", numMessages)
+}
